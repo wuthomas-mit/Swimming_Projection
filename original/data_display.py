4835675069ca7fe673765885fbff1811653dc9b0
puppyswan <92048050+puppyswan@users.noreply.github.com>
2023-11-26 00:36:35 -0500

    new

---
+++ original/data_display.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+import json
+import numpy as np
+from scipy.optimize import curve_fit
+
+# Define the logarithmic function
+def logarithmic_func(x, a, b):
+    return a * np.log(x) + b
+
+# Load the combined data from the JSON file
+with open('original/data/combined_data.json', 'r') as file:
+    combined_data = json.load(file)
+
+# Dictionary to store the parameters for each event
+all_log_fit_params = {}
+
+# Iterate through each person
+for person_key, person_data in combined_data.items():
+    gender = person_data['gender']
+    for event_name, event_data in person_data['events'].items():
+        if len(event_data['swims']) > 7:
+            data_points = []
+            for date_key, time_value in event_data['swims'].items():
+                date = datetime.strptime(date_key, '%Y%m%d').date()
+                data_points.append((date, float(time_value)))
+
+            data_points.sort(key=lambda x: x[0])
+            sorted_dates = [x[0].strftime('%b %d %Y') for x in data_points]
+            sorted_times = [x[1] for x in data_points]
+
+            plt.figure(figsize=(12, 8)) 
+            plt.plot(sorted_dates, sorted_times, '-o', label='Data Points')
+
+            # Fit the function
+            x = np.arange(1, len(sorted_dates) + 1)  # Using 1-based index for the swims
+            popt, _ = curve_fit(logarithmic_func, x, sorted_times)
+
+            if event_name in all_log_fit_params:
+                all_log_fit_params[event_name].append(popt)
+            else:
+                all_log_fit_params[event_name] = [popt]
+
+            # Plot the fitted curve
+            plt.plot(sorted_dates, logarithmic_func(x, *popt), label='Logarithmic Fit')
+
+            plt.xlabel('Date')
+            plt.ylabel('Time (seconds)')
+            plt.title(f'Performance Over Time for Person {person_key} - Event: {event_name} - Gender: {gender}')
+
+            # Set y-axis limits dynamically
+            plt.ylim(min(sorted_times) * 0.985, max(sorted_times) * 1.015)
+
+            plt.xticks(rotation=45)  # Rotate x-axis labels for better visibility
+            plt.legend()
+            plt.show()
+
+# Calculate the average parameters for each event
+average_log_fit_params = {}
+for event_name, params_list in all_log_fit_params.items():
+    average_params = np.mean(params_list, axis=0)
+    average_log_fit_params[event_name] = average_params
---
+++ synth_control.py
@@ -0,0 +1,122 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pysyncon import Dataprep, Synth
+
+
+
+# Load your data from Excel
+file_path = '/Users/thomaswu/Documents/Programming/Swimming_Projection/combined_output_50FREE.xlsx'
+df = pd.read_excel(file_path)
+
+# Select the fastest swim per year of age
+fastest_swims = df.sort_values('Time').groupby(['Name', 'Age']).head(1)
+
+# Pivot the data
+pivoted_df = fastest_swims.pivot_table(index='Name', columns='Age', values='Time', aggfunc='first').fillna(np.NaN)
+
+
+'''
+
+# Count non-NaN values for each column
+non_nan_counts = pivoted_df.notna().sum(axis=0)
+
+# Sort the columns based on the number of non-NaN values in descending order
+sorted_columns = non_nan_counts.sort_values(ascending=False)
+
+# Display the result
+print(sorted_columns)
+
+'''
+
+# Specify the age range you want to include
+age_range_start = 9
+age_range_end = 21
+
+# Convert column names to integers and filter columns based on the age range
+selected_columns = pivoted_df.loc[:, (pivoted_df.columns.astype(int) >= age_range_start) & (pivoted_df.columns.astype(int) <= age_range_end)]
+
+
+# # Filter swimmers with times for all ages in the range
+# filtered_swimmers = selected_columns.loc[:, (selected_columns.columns >= age_range_start) & (selected_columns.columns <= age_range_end)].astype(bool).all(axis=1)
+
+# # Keep only the rows (swimmers) that have all times
+# swimmers_all_times = selected_columns[filtered_swimmers]
+
+'''
+# Convert the DataFrame to a numpy array
+data_matrix = swimmers_all_times.values
+
+# Perform SVD
+U, Sigma, VT = np.linalg.svd(data_matrix, full_matrices=False)
+
+# U: Left singular vectors
+# Sigma: Singular values
+# VT: Transpose of right singular vectors
+
+
+# Number of singular values to retain (choose based on the elbow method or other criteria)
+k = 1
+
+# Retain only the top k singular values and vectors
+U_k = U[:, :k]
+Sigma_k = np.diag(Sigma[:k])
+VT_k = VT[:k, :]
+
+
+# Assuming new_swimmer_data is a new swimmer's data
+new_swimmer_data = pd.DataFrame({
+    'Name': "INPUT",
+    'Age': [10, 11, 12, 13, 14, 15, 16],
+    'Time': [62.13, 58.96, 53.99, 49.51, 47.5, 45.56, 43.82],  # Fill in the actual time data for the new swimmer
+})
+
+'''
+# new strat start
+# Assuming 'pivoted_df' is the pivoted DataFrame
+stacked_df = selected_columns.stack().reset_index()
+
+# Rename the columns for clarity
+stacked_df.columns = ['Name', 'Age', 'Time']
+
+'''
+#Creating Input Swimmer
+new_ages = [10, 11, 12, 13, 14, 15, 16, 17]
+# new_times = [62.13, 58.96, 53.99, 49.51, 47.5, 45.56, 43.82]
+new_times = [28.99, 27.42, 26.05, 23.67, 22.71, 22.4, 21.75, 21.04]
+
+# Create a DataFrame from the new data
+new_data = {'Name': 'INPUT', 'Age': new_ages, 'Time': new_times}
+new_rows_df = pd.DataFrame(new_data)
+
+# Append the new DataFrame to the existing DataFrame
+stacked_df = pd.concat([stacked_df, new_rows_df], ignore_index=True)
+'''
+
+names_list = stacked_df['Name'].unique().tolist()
+# names_list.remove("INPUT")
+remove = ['Aaron Sequeira', 'Abe Townley', 'Antonio Milin', 'Ben Hilfinger', 'Daniel Baltes', 'Diego Mas', 'Drew Hawthorne', 'Jerard Jacinto', 'Kent Olsen-Stavrakas', 'Kraig Bray', 'Matthew Namakonov', 'Mike Atanasoff', 'Ryan Perham', 'Tomas Koski']
+names_list = [name for name in names_list if name not in remove]
+
+
+dataprep_train = Dataprep(
+    foo=stacked_df,
+    predictors= ["Time"],
+    predictors_op= "mean",
+    time_predictors_prior=range(14,18),
+    dependent="Time",
+    unit_variable="Name",
+    time_variable="Age",
+    treatment_identifier="Aaron Sequeira",
+    controls_identifier= names_list,
+    time_optimize_ssr=range(14,18),
+)
+
+synth_train = Synth()
+synth_train.fit(dataprep=dataprep_train)
+# synth_train.fit(dataprep=dataprep_train, optim_method="Nelder-Mead", optim_initial="equal")
+for weight in synth_train.weights():
+    print(weight)
+
+synth_train.path_plot(time_period=range(10, 22), treatment_time= 17)
+synth_train.gaps_plot(time_period=range(10, 22), treatment_time= 17)
